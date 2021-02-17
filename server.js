348202dad14731277d727c55e7f759e4680881a3
Malachi <malachialusa@gmail.com>
2021-02-17 16:55:00 -0700

    I added server functionality, and I added the user class, column, and api points. Also tested routes

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
@@ -15,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Server Running'));
