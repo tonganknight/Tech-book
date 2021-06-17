a44ae91786828af7e47ba421764200e0eff0ab7d
Malachi <malachialusa@gmail.com>
2021-06-17 14:54:46 -0600

    This is the first front end update.. more to come

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -3,7 +3,7 @@ async function editFormHandler(event) {
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
   const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+  window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
